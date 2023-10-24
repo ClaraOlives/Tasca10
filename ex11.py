e63b5fa541247e14f4dba144ad6845056f46e2a4
ClaraOlives <colives19533@iesjoanramis.org>
2023-10-24 14:00:29 +0200

    ex11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+def menu_principal():
+    print("""
+        Menú principal:
+        1. Calculadora enters
+        2. Calculadora reals
+        3. Sortir
+          """)
+    x = int(input("Eligeixi una opció"))
