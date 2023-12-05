c575bc5b37d894fc9372769befe2407432e3bed4
ClaraOlives <148210559+ClaraOlives@users.noreply.github.com>
2023-12-05 12:51:40 +0100

    Ex15.py

--- Ex15.py
+++ Ex15.py
@@ -1,23 +1,18 @@
-def lon(a):
-    t = 0
-    for e in a:
-        t += 1
-    return t
+def lon():
+    a = [1, 2, 3, 4] 
+    print(a)
+    A = len(a)
+    print("Aquesta llista conté {} elements. \n".format(A))
+    b = [5,6,7,8,9,10,11,12]
+    print(b)
+    B = len(b)
+    print("Aquesta llista conté {} elements. \n".format(B))
+    c = ["Hola", "Bon dia", "Vocals"]
+    print(c)
+    C = len(c)
+    print("Aquesta llista conté {} elements. \n".format(C))
 
-
-a = [1, 2, 3, 4] 
-print(a)
-A = len(a)
-print("Aquesta llista conté {} elements. \n".format(A))
-b = [5,6,7,8,9,10,11,12]
-print(b)
-B = len(b)
-print("Aquesta llista conté {} elements. \n".format(B))
-c = ["Hola", "Bon dia", "Vocals"]
-print(c)
-C = len(c)
-print("Aquesta llista conté {} elements. \n".format(C))
-
+lon()
 
 
 
